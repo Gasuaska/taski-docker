9fc44bf6522eab9b2839b886b22be68de666025d
Gasuaska <gasuaska2@gmail.com>
2025-07-26 12:04:41 +0500

    hz

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
                  '84.201.176.175',
-                 'gasuaska-taski.zapto.org']
+                 'gasuaska-kittygram.zapto.org']
 
 
 # Application definition
